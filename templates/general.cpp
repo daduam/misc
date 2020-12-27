2ebfdbf038179ccbb71e919b7e747e702eb47707
PotatoPcGuy.ts <josephampadu549@gmail.com>
2020-12-27 09:35:38 +0000

    Create general.cpp

---
+++ templates/general.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define REP(i, a, b) for (int i = a; i < b; i++)
+#define RREP(i, a, b) for (int i = a; i >= b; i--)
+#define len(x) (int)(x).size()
+#define print(x) cout << (x) << "\n"
+
+void solve() {
+
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  solve();
+}
