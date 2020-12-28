15751abbdba96215af61244d118bb3e2cb7fe7dd
PotatoPcGuy.ts <josephampadu549@gmail.com>
2020-12-28 20:18:27 +0000

    Update general.cpp

--- templates/general.cpp
+++ templates/general.cpp
@@ -1,18 +1,28 @@
 #include <bits/stdc++.h>
+#pragma GCC optimize("O3")
 using namespace std;
+
 typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<ii> vii;
+
 #define REP(i, a, b) for (int i = a; i < b; i++)
 #define RREP(i, a, b) for (int i = a; i >= b; i--)
 #define len(x) (int)(x).size()
 #define print(x) cout << (x) << "\n"
+#define all(x) x.begin(), x.end()
+
+#define MOD 1000000007
 
 void solve() {
 
 }
 
 int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(0);
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
 
   solve();
 }
