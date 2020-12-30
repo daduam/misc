92ff82a9ea4ddf63e5cd3e7f916a35ec53ecc754
PotatoPcGuy.ts <josephampadu549@gmail.com>
2020-12-30 03:58:03 +0000

    still from same article

--- templates/main.rs
+++ templates/main.rs
@@ -1,25 +1,40 @@
-#[allow(unused_imports)]
+#![allow(unused_imports)]
 use std::cmp::{max, min};
-use std::io::{stdin, stdout, BufWriter, Write};
+use std::io::{self, prelude::*};
+
+fn solve<R: BufRead, W: Write>(mut input: Scanner<R>, mut w: W) {
+    writeln!(w, "come to compete?");
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let input = Scanner::from(stdin.lock());
+    let writer = io::BufWriter::new(stdout.lock());
+    solve(input, writer);
+}
 
 #[derive(Default)]
-struct Scanner {
+struct Scanner<B> {
+    reader: B,
     buffer: Vec<String>,
 }
-impl Scanner {
-    fn next<T: std::str::FromStr>(&mut self) -> T {
+impl<R: BufRead> Scanner<R> {
+    pub fn token<T: std::str::FromStr>(&mut self) -> T {
         loop {
             if let Some(token) = self.buffer.pop() {
                 return token.parse().ok().expect("Failed parse");
             }
             let mut input = String::new();
-            stdin().read_line(&mut input).expect("Failed read");
+            self.reader.read_line(&mut input).expect("Failed read");
             self.buffer = input.split_whitespace().rev().map(String::from).collect();
         }
     }
 }
-
-fn main() {
-    let mut scan = Scanner::default();
-    let out = &mut BufWriter::new(stdout());
+impl<R: BufRead> From<R> for Scanner<R> {
+    fn from(reader: R) -> Self {
+        Self {
+            reader,
+            buffer: Vec::new(),
+        }
+    }
 }
