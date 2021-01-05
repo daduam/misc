4257e4a6dbca542e4709436458bd39da9753d41b
PotatoPcGuy.ts <josephampadu549@gmail.com>
2021-01-05 11:42:58 +0000

    Create testcases.cpp

---
+++ templates/testcases.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  
+  long long t;
+  cin >> t;
+  while(t--) {
+    solve();
+  }
+}
+    
