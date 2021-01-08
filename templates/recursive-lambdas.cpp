a832ca6b4a58482874ba3c46801cdffc959b1459
PotatoPcGuy.ts <josephampadu549@gmail.com>
2021-01-08 13:11:09 +0000

    Create recursive-lambdas.cpp

---
+++ templates/recursive-lambdas.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+namespace std {
+  // http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2016/p0200r0.html
+  template<class Fun>
+  class y_combinator_result {
+    Fun fun_;
+  public:
+    template<class T>
+    explicit y_combinator_result(T&& fun) :fun_(std::forward<T>(fun)) {}
+
+    template<class ...Args>
+    decltype(auto) operator()(Args &&...args) {
+      return fun_(std::ref(*this), std::forward<Args>(args)...);
+    }
+  };
+
+  template<class Fun>
+  decltype(auto) y_combinator(Fun&& fun) {
+    return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun));
+  }
+} // namespace std
+
+void solve() {
+  using namespace std;
+}
+
+int main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  
+  solve();
+}
